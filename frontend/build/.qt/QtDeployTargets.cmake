c19f95dbc6d0207ed2182fde5c7e7da132de234d
Sunix1204pro <phamminhnhat1204@gmail.com>
2026-07-01 11:51:55 +0700

    add number people for room

--- frontend/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_HotelManagement_FILE /Users/sunix/Documents/Hotel-RoomBooking-Management-System_OOP-Project_HCMUS/frontend/build/HotelManagement)
-set(__QT_DEPLOY_TARGET_HotelManagement_TYPE EXECUTABLE)
